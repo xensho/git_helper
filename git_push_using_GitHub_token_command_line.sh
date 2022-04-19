500834e82d274d9a29c8a715605dbebdcb67300c
Stancho <stanko.opacic@gmail.com>
2022-04-19 17:15:55 +0200

    Create git_push_using_GitHub_token_command_line.sh
    Adding explanation how to use GitHub Token.

---
+++ git_push_using_GitHub_token_command_line.sh
@@ -0,0 +1,10 @@
+# Note:
+# Personal Access Tokens (PAT) can only be used for HTTPS git operations. Switch your repository to HTTPS if it is using SSH.
+
+# Once you have a token, you can use it instead of your password when performing git push operations over HTTPS as below:
+
+
+git push https://<GITHUB_ACCESS_TOKEN>@github.com/<GITHUB_USERNAME>/<REPOSITORY_NAME>.git
+
+
+# Note: Remember to replace <GITHUB_ACCESS_TOKEN>, <GITHUB_USERNAME>, <REPOSITORY_NAME> with your token, username and repository.
